77e02c26d184a666d271e9579a573739a141f793
Sophia Di <backwardsxmarathon@gmail.com>
2018-11-27 18:18:20 -0500

    Add user model and registration

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
@@ -12,8 +21,13 @@ mongoose
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
-const port = process.env.PORT || 8888;
-
 app.get('/', (req, res) => res.send('Hello'));
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 8888;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
